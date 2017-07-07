3e9260e43a2651a882b1d53b3a0fd668a4325d52
kodeir <kodeir@gmail.com>
2017-07-07 19:35:34 +0300

    database changes

--- src/main/java/com/kodeir/kcommons/database/DefaultDatabase.java
+++ src/main/java/com/kodeir/kcommons/database/DefaultDatabase.java
@@ -2,61 +2,132 @@ package com.kodeir.kcommons.database;
 
 import com.kodeir.kcommons.log.KLogger;
 
-import java.sql.ResultSet;
+import java.sql.*;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DefaultDatabase implements Database{
 
     private static final Logger logger = new KLogger(DefaultDatabase.class.getName());
 
+    private Connection connection = null;
+    private Statement statement = null;
+
+    public DefaultDatabase(String dbUrl, String user, String password){
+        setConnection(dbUrl, user, password);
+    }
 
     @Override
-    public boolean setConnection(String database, String user, String password) {
-        return false;
+    public boolean setConnection(String dbUrl, String user, String password) {
+        try {
+            connection = DriverManager.getConnection(dbUrl, user, password);
+            return true;
+        } catch (SQLException e) {
+            return printSqlException(e, "Failed to open connection to " + dbUrl + " with user = " + user + "!");
+        }
     }
 
     @Override
     public boolean closeConnection() {
-        return false;
+        closeStatement();
+        try{
+            if (connection != null) {
+                connection.close();
+                logger.log(Level.FINE, "Connection was closed successfully.");
+                return true;
+            } else {
+                logger.log(Level.FINE, "Connection was not opened.");
+                return true;
+            }
+        } catch (SQLException e){
+            return printSqlException(e, "Failed to close a connection.");
+        }
     }
 
     @Override
     public boolean setStatement() {
-        return false;
+        try {
+            statement = connection.createStatement();
+            return true;
+        } catch (SQLException e) {
+            return printSqlException(e, "Failed to create a statement.");
+        }
     }
 
     @Override
     public boolean closeStatement() {
-        return false;
+        try{
+            if (statement != null) {
+                statement.close();
+                logger.log(Level.FINE, "Statement was closed successfully.");
+                return true;
+            } else {
+                logger.log(Level.FINE, "Statement was not opened.");
+                return true;
+            }
+        } catch (SQLException e){
+            return printSqlException(e, "Failed to close a statement.");
+        }
     }
 
     @Override
     public boolean runExecuteUpdateQuery(String sql) {
-        return false;
+        try {
+            statement.executeUpdate(sql);
+            return true;
+        } catch (SQLException e) {
+            return printSqlException(e, "ExecuteUpdateQuery failed");
+        }
     }
 
     @Override
     public boolean runExecute(String sql) {
-        return false;
+        try {
+            statement.execute(sql);
+            return true;
+        } catch (SQLException e) {
+            return printSqlException(e, "ExecuteQuery failed");
+        }
     }
 
     @Override
     public ResultSet runSelectQuery(String sql) {
-        return null;
+        try {
+            return statement.executeQuery(sql);
+        } catch (SQLException e) {
+            printSqlException(e, "SelectQuery failed");
+            return null;
+        }
     }
 
     @Override
     public boolean connectionIsNull() {
-        return false;
+        return connection == null;
     }
 
     @Override
     public boolean statementIsNull() {
-        return false;
+        return statement == null;
     }
 
     @Override
     public boolean commit() {
+        try {
+            connection.commit();
+            return true;
+        } catch (SQLException e) {
+            return printSqlException(e, "Commit failed");
+        }
+    }
+
+    private boolean printSqlException(SQLException e, String message){
+        logger.log(Level.SEVERE, message
+                        + "\n"
+                        + "Error code: " + e.getErrorCode()
+                        + "\n"
+                        + e.getMessage()
+                        + "\n"
+                , e);
         return false;
     }
 }
